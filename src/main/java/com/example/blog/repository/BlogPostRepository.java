b981cb1778bb47601867624f631f4dc2bc5e1dc1
eccentricharry <harinathrao13@gmail.com>
2025-12-29 12:11:30 +0530

    amended repos

--- src/main/java/com/example/blog/repository/BlogPostRepository.java
+++ src/main/java/com/example/blog/repository/BlogPostRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
     boolean existsByTitle(String title);
+    BlogPost findBySlug(String slug);
 }
